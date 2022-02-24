e5e362681c7a0623d2d0e1fd0c9e7db69e1782f6
Hillel Arnold <helrond@hotmail.com>
2022-02-24 18:40:52 -0500

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     long_description_content_type="text/markdown",
     author="Rockefeller Archive Center",
     author_email="archive@rockarch.org",
-    version="0.1.0",
+    version="0.1.1",
     license='MIT',
     packages=find_packages(),
     zip_safe=False,
